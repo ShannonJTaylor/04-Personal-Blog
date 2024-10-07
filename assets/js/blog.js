d4c395b9645983f0e20f00fa0e0b21b1d9e4c7e1
Shannon Taylor <shannon.taylor@zoetis.com>
2024-10-07 18:40:09 -0400

    added logic code to the js logic file, updated index file with back button functionality, updated the blog js file.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,40 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
+const main= document.getElementById('main');
+const backButton = document.getElementById('back-button');
 
 // TODO: Create a function that builds an element and appends it to the DOM
+const newElement = function (type, className, text) {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  element.textContent = text;
+  main.appendChild(element);
+};
 
 // TODO: Create a function that handles the case where there are no blog posts to display
+//bring in the error message here, it's in a p tag
+const noPosts = function () {
+  newElement('p', 'no-posts', 'No blog posts to display.');
+};
 
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
-
+const renderBlogList = function () {
+  const blogData = readLocalStorage();
+  if (blogData.length === 0) {
+    noPosts();
+  } else {
+    blogData.forEach((post) => {
+      newElement('div', 'blog-post', post.title);
+    });
+  }
+};
 // TODO: Call the `renderBlogList` function
-
+function renderBlogList() {
+    console.log(`renderBlogList`);
+}
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
+let redirectURL = '/index.html';
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,10 +2,17 @@
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
-
+function readLocalStorage() {
+  const blogData = JSON.parse(localStorage.getItem('blogData'));
+  return blogData ? blogData : [];
+}
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
-
+function storeLocalStorage(newData) {
+  const blogData = readLocalStorage();
+  blogData.push(newData);
+  localStorage.setItem('blogData', JSON.stringify(blogData));
+}
 
 // ! Use the following function whenever you need to redirect to a different page
 
